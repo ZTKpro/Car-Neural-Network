767231326d867f4c8a1c740bc4a99da354ba9ab9
Damian Szmurlo <d.szmurlo@forbetterfuture.pl>
2022-12-24 23:54:31 +0100

    main- add sensor

--- car.js
+++ car.js
@@ -11,11 +11,13 @@ class Car {
     this.friction = 0.05;
     this.angle = 0;
 
+    this.sensor = new Sensor(this);
     this.controls = new Controls();
   }
 
-  update() {
+  update(roadBorders) {
     this.#move();
+    this.sensor.update(roadBorders);
   }
 
   #move() {
@@ -43,9 +45,8 @@ class Car {
       this.speed = 0;
     }
 
-    if (this.speed !== 0) {
+    if (this.speed != 0) {
       const flip = this.speed > 0 ? 1 : -1;
-
       if (this.controls.left) {
         this.angle += 0.03 * flip;
       }
@@ -68,5 +69,7 @@ class Car {
     ctx.fill();
 
     ctx.restore();
+
+    this.sensor.draw(ctx);
   }
 }
